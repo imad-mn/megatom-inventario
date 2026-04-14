d6a28e8c86f596c59ebb4e8fb7ff0231e5e034b4
Imad Marmoud <imadmarmoud@gmail.com>
2026-04-14 14:59:43 -0600

    Ajustar imagen

--- src/paginas/PaginaHome.vue
+++ src/paginas/PaginaHome.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-
-</script>
 <template>
-  <div>
-    <img src="/Megatom-Home.png?url" alt="Logo" class="justify-self-center" />
+  <div class="h-[calc(100vh-6rem)] flex items-center justify-center overflow-hidden">
+    <img src="/Megatom-Home.png?url" alt="Logo" class="max-h-full max-w-full object-contain" />
   </div>
 </template>
